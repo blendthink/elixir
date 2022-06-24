5290d3ede2110788b3a99b159d8fc8a72a306a52
blendthink <info@blendthink.dev>
2022-06-25 00:30:35 +0900

    initial commit

---
+++ bin/sign.dart
@@ -0,0 +1,5 @@
+import 'package:sign/sign.dart' as sign;
+
+void main(List<String> arguments) {
+  print('Hello world: ${sign.calculate()}!');
+}
---
+++ test/sign_test.dart
@@ -0,0 +1,8 @@
+import 'package:sign/sign.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
