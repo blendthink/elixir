0a0238f6313a5b3a9727ee047396bc8ebf78b45e
blendthink <info@blendthink.dev>
2022-06-30 09:13:06 +0900

    add DartRepository class

---
+++ lib/data/repository/dart.dart
@@ -0,0 +1,16 @@
+import 'package:sign/data/converter/analyze.dart';
+import 'package:sign/data/model/indicate.dart';
+import 'package:sign/data/source/process.dart';
+
+class DartRepository {
+  final ProcessRunner _runner;
+
+  DartRepository({
+    ProcessRunner runner = const ProcessRunner('dart'),
+  }) : _runner = runner;
+
+  Future<List<Indicate>> getIndicates() async {
+    final result = await _runner.run(['analyze']);
+    return AnalyzeConverter().convert(result);
+  }
+}
