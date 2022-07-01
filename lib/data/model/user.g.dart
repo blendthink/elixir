248ea51e14d64e65304b5cf5d7192892ce636828
blendthink <info@blendthink.dev>
2022-07-01 22:13:18 +0900

    add User class

---
+++ lib/data/model/user.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$_User _$$_UserFromJson(Map<String, dynamic> json) => _$_User(
+      login: json['login'] as String,
+      id: json['id'] as int,
+    );
+
+Map<String, dynamic> _$$_UserToJson(_$_User instance) => <String, dynamic>{
+      'login': instance.login,
+      'id': instance.id,
+    };
