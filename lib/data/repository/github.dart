21a48c845ef2ab2e5d66d1ce75b4502441f0fcb4
blendthink <info@blendthink.dev>
2022-07-01 22:41:45 +0900

    add getReviewComments method

--- lib/data/repository/github.dart
+++ lib/data/repository/github.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:sign/data/model/review_comment.dart';
 import 'package:sign/data/model/user.dart';
 import 'package:sign/data/source/process.dart';
 
@@ -22,5 +23,23 @@ class GitHubRepository {
     ]);
 
     return User.fromJson(jsonDecode(result));
+  }
+
+  /// gh api \
+  ///   -H "Accept: application/vnd.github.v3+json" \
+  ///   /repos/{repo}/pulls/{num}/comments
+  Future<Iterable<ReviewComment>> getReviewComments({
+    required String repo,
+    required int num,
+  }) async {
+    final result = await _runner.run([
+      'api',
+      '-H',
+      'Accept: application/vnd.github.v3+json',
+      '/repos/$repo/pulls/$num/comments',
+    ]);
+
+    return (jsonDecode(result) as List)
+        .map((json) => ReviewComment.fromJson(json));
   }
 }
