32cfa0a3b7a4032de60085edfed9720507a3f411
blendthink <info@blendthink.dev>
2022-07-01 18:42:02 +0900

    add GitRepository class

---
+++ lib/data/repository/git.dart
@@ -0,0 +1,32 @@
+import 'package:sign/data/source/process.dart';
+
+class GitRepository {
+  final ProcessRunner _runner;
+
+  const GitRepository({
+    ProcessRunner runner = const ProcessRunner('git'),
+  }) : _runner = runner;
+
+  /// git log {base}..{head} --format=%H -n 1 -L {line},+1:{path} | grep -E '^([0-9|a-z]){40}'
+  /// Result: e.q. 6d87458a8089ae5d025b087bfb9b0809d2060411
+  Future<String> getLatestCommitId({
+    required String base,
+    required String head,
+    required String path,
+    required int line,
+  }) async {
+    return _runner.run([
+      'log',
+      '$base..$head',
+      '--format=%H',
+      '-n',
+      '1',
+      '-L',
+      '$line,+1:$path',
+      '|',
+      'grep',
+      '-E',
+      "'^([0-9|a-z]){40}'",
+    ]);
+  }
+}
