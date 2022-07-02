098cffc96428dfd712913dfdab7428dc5d95d8d9
blendthink <info@blendthink.dev>
2022-07-02 14:20:11 +0900

    fix add command

--- lib/cli/runner.dart
+++ lib/cli/runner.dart
@@ -1,4 +1,5 @@
 import 'package:args/command_runner.dart';
+import 'package:elixir/cli/command/run.dart';
 import 'package:elixir/cli/extension.dart';
 import 'package:elixir/cli/flag/version.dart';
 import 'package:elixir/gen/pubspec.dart';
@@ -13,6 +14,7 @@ class ElixirCommandRunner extends CommandRunner {
     argParser.addFlags([
       VersionFlag(),
     ]);
+    addCommand(RunCommand());
   }
 
   @override
