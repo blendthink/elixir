6b45b450f3b4f099e6bd60a73ceb7d4ae75d8343
blendthink <admin@blendthink.dev>
2022-08-19 22:21:17 +0900

    fix GetIndicatesUseCase

--- lib/data/model/indicate.dart
+++ lib/data/model/indicate.dart
@@ -1,19 +1,13 @@
-import 'package:freezed_annotation/freezed_annotation.dart';
+class Indicate {
+  const Indicate({
+    required this.body,
+    required this.commitId,
+    required this.path,
+    required this.line,
+  });
 
-part 'indicate.freezed.dart';
-
-part 'indicate.g.dart';
-
-@freezed
-abstract class Indicate with _$Indicate {
-  const factory Indicate({
-    required String diagHash,
-    required String body,
-    required String commitId,
-    required String path,
-    required int line,
-  }) = _Indicate;
-
-  factory Indicate.fromJson(Map<String, Object?> json) =>
-      _$IndicateFromJson(json);
+  final String body;
+  final String commitId;
+  final String path;
+  final int line;
 }
