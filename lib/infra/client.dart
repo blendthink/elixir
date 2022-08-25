83c5f8ab13080f23cc9325cfb7376b1d66c6ef63
blendthink <admin@blendthink.dev>
2022-08-25 17:53:05 +0900

    add getRequest, deleteRequest to GitHubClient

--- lib/infra/client.dart
+++ lib/infra/client.dart
@@ -21,20 +21,55 @@ class GitHubClient extends BaseClient {
     return _client.send(request);
   }
 
+  Future<String> getRequest({
+    required String path,
+  }) async {
+    final url = Uri.https(_authority, path);
+    final response = await super.get(url);
+    _checkStatusCode(response.statusCode);
+    return response.body;
+  }
+
   Future<String> postRequest({
     required String path,
     required String data,
   }) async {
     final url = Uri.https(_authority, path);
     final response = await super.post(url, body: data);
-    final statusCode = response.statusCode;
+    _checkStatusCode(response.statusCode);
+    return response.body;
+  }
+
+  Future<void> deleteRequest({
+    required String path,
+  }) async {
+    final url = Uri.https(_authority, path);
+    final response = await super.get(url);
+    _checkStatusCode(response.statusCode);
+  }
+
+  void _checkStatusCode(int statusCode) {
     switch (statusCode) {
       case 200:
-        return response.body;
+      case 201:
+      case 204:
+        return;
+      case 400:
+        throw const BadRequest();
+      case 401:
+        throw const RequiresAuthentication();
       case 403:
         throw const Forbidden();
+      case 404:
+        throw const NotFound();
       case 422:
         throw const ValidationFailed();
+      case 429:
+        throw const TooManyRequests();
+      case 500:
+        throw const InternalError();
+      case 503:
+        throw const ServiceUnavailable();
       default:
         throw const Unknown();
     }
